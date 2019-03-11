80aaf61f9d2284642679fc4f162067e9d41ed305
tequila <114778836@qq.com>
2019-03-11 18:45:44 +0800

    add middleware module

---
+++ src/middleware/types/consensus_helper.go
@@ -0,0 +1,53 @@
+package types
+
+import (
+	"common"
+	"math/big"
+)
+
+type GenesisInfo struct {
+	Group  Group
+	VrfPKs [][]byte
+	Pks    [][]byte
+}
+
+/*
+	共识接口合集
+*/
+type ConsensusHelper interface {
+
+	//generate genesis group and member pk info
+	GenerateGenesisInfo() *GenesisInfo
+
+	//vrf prove to value
+	VRFProve2Value(prove *big.Int) *big.Int
+
+	//bonus for proposal a block
+	ProposalBonus() *big.Int
+
+	//bonus for packing one bonus transaction
+	PackBonus() *big.Int
+
+	//calcaulate the blockheader's qn
+	//it needs to be equal to the blockheader's totalQN - preHeader's totalQN
+	CalculateQN(bh *BlockHeader) uint64
+
+	//generate verify hash of the block for current node
+	VerifyHash(b *Block) common.Hash
+
+	//check the prove root hash for weight node when add block on chain
+	CheckProveRoot(bh *BlockHeader) (bool, error)
+
+	//check the new block
+	//mainly verify the cast legality, group signature
+	VerifyNewBlock(bh *BlockHeader, preBH *BlockHeader) (bool, error)
+
+	//verify the blockheader: mainly verify the group signature
+	VerifyBlockHeader(bh *BlockHeader) (bool, error)
+
+	//check group
+	CheckGroup(g *Group) (bool, error)
+
+	//verify bonus transaction
+	VerifyBonusTransaction(tx *Transaction) (bool, error)
+}
---
+++ src/middleware/types/receipt.go
@@ -0,0 +1,79 @@
+
+package types
+
+import (
+	"bytes"
+	"fmt"
+	"unsafe"
+
+	"common"
+)
+
+//go:generate gencodec -type Receipt -field-override receiptMarshaling -out gen_receipt_json.go
+
+var (
+	receiptStatusFailed     = []byte{}
+	receiptStatusSuccessful = []byte{0x01}
+)
+
+const (
+	ReceiptStatusFailed = uint(0)
+
+	ReceiptStatusSuccessful = uint(1)
+)
+
+type Receipt struct {
+	PostState         []byte `json:"-"`
+	Status            uint   `json:"status"`
+	CumulativeGasUsed uint64 `json:"cumulativeGasUsed"`
+	Bloom             Bloom  `json:"-"`
+	Logs              []*Log `json:"logs"`
+
+	TxHash          common.Hash    `json:"transactionHash" gencodec:"required"`
+	ContractAddress common.Address `json:"contractAddress"`
+}
+
+func NewReceipt(root []byte, failed bool, cumulativeGasUsed uint64) *Receipt {
+	r := &Receipt{PostState: common.CopyBytes(root), CumulativeGasUsed: cumulativeGasUsed}
+	if failed {
+		r.Status = ReceiptStatusFailed
+	} else {
+		r.Status = ReceiptStatusSuccessful
+	}
+	return r
+}
+
+func (r *Receipt) setStatus(postStateOrStatus []byte) error {
+	switch {
+	case bytes.Equal(postStateOrStatus, receiptStatusSuccessful):
+		r.Status = ReceiptStatusSuccessful
+	case bytes.Equal(postStateOrStatus, receiptStatusFailed):
+		r.Status = ReceiptStatusFailed
+	case len(postStateOrStatus) == len(common.Hash{}):
+		r.PostState = postStateOrStatus
+	default:
+		return fmt.Errorf("invalid receipt status %x", postStateOrStatus)
+	}
+	return nil
+}
+
+func (r *Receipt) Size() common.StorageSize {
+	size := common.StorageSize(unsafe.Sizeof(*r)) + common.StorageSize(len(r.PostState))
+
+	size += common.StorageSize(len(r.Logs)) * common.StorageSize(unsafe.Sizeof(Log{}))
+	for _, log := range r.Logs {
+		size += common.StorageSize(len(log.Topics)*common.HashLength + len(log.Data))
+	}
+	return size
+}
+
+func (r *Receipt) String() string {
+	if len(r.PostState) == 0 {
+		return fmt.Sprintf("receipt{status=%d cgas=%v bloom=%x logs=%v}", r.Status, r.CumulativeGasUsed, r.Bloom, r.Logs)
+	}
+	return fmt.Sprintf("receipt{med=%x cgas=%v bloom=%x logs=%v}", r.PostState, r.CumulativeGasUsed, r.Bloom, r.Logs)
+}
+
+type Receipts []*Receipt
+
+func (r Receipts) Len() int { return len(r) }
