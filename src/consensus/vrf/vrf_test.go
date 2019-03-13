1a2d4c87c4e8733da1bdce05aad3bea9dfddfc9c
FlyingSmallBear <1581487855@qq.com>
2019-03-13 17:40:14 +0800

    vrf init

--- src/consensus/vrf/vrf_test.go
+++ src/consensus/vrf/vrf_test.go
@@ -2,29 +2,15 @@ package vrf
 
 import (
 	"testing"
-	"math/big"
 	"fmt"
 
 	"x/src/consensus/base"
 )
 
 func TestVRF_prove(t *testing.T) {
-	//total := new(big.Int)
-	pk, sk, _ := VRF_GenerateKey(nil)
-	for i := 0; i < 1000000000; i ++ {
-		pi, _ := VRF_prove(pk, sk, base.NewRand().Bytes())
-		bi := new(big.Int).SetBytes(pi)
-		if bi.Sign() < 0 {
-			fmt.Errorf("error bi %d", bi )
-			break
-		}
-		//total = total.Add(total, bi)
-		//if total.Sign() < 0 {
-		//	fmt.Errorf("error total %d", total)
-		//	break
-		//}
-		if i%10000 == 0 {
-			fmt.Printf("%d total: %d\n", i, bi)
-		}
-	}
+	pk, sk, _ := VRFGenerateKey(nil)
+	msg := base.NewRand().Bytes()
+	prove, _ := VRFGenProve(pk, sk, msg)
+	result, _ := VRFVerify(pk, prove, msg)
+	fmt.Printf("VRF verify result:%t\n", result)
 }
