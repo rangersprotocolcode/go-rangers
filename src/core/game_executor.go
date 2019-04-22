59455d6cf9cc27e0ae8641de91ce88d283d932dc
tequila <114778836@qq.com>
2019-04-22 11:27:49 +0800

    client message

--- src/core/game_executor.go
+++ src/core/game_executor.go
@@ -6,31 +6,36 @@ import (
 	"strconv"
 	"x/src/common"
 	"encoding/json"
+	"x/src/network"
+	"x/src/middleware/notify"
 )
 
 type GameExecutor struct {
 }
 
-func (executor *GameExecutor) Tx(txRaw types.Transaction) ([]byte, error) {
-	var message []byte
+func (executor *GameExecutor) Tx(message notify.ClientTransactionMessage) error {
+	var result []byte
+	txRaw := message.Tx
 	// execute state machine transaction
 	if txRaw.Type == types.TransactionTypeOperatorEvent {
 		payload := string(txRaw.Data)
 		outputMessage := statemachine.Docker.Process(txRaw.Target, "operator", strconv.FormatUint(txRaw.Nonce, 10), payload)
 
-		message, _ = json.Marshal(outputMessage)
+		result, _ = json.Marshal(outputMessage)
 
 	}
 
 	if err := executor.sendTransaction(&txRaw); err != nil {
-		return nil, err
+		return err
 	}
 
 	if txRaw.Type == types.TransactionTypeOperatorEvent {
 		GetBlockChain().GetTransactionPool().AddExecuted(&txRaw)
 	}
 
-	return message, nil
+	network.GetNetInstance().SendToClient(message.UserId, network.Message{Body: result})
+
+	return nil
 
 }
 
