103877208179f0ecddf14762071ecea81871727a
FlyingSmallBear <1581487855@qq.com>
2019-04-23 15:40:05 +0800

    pre start.sh modified

--- deploy/pre/start.sh
+++ deploy/pre/start.sh
@@ -8,9 +8,6 @@ instance_index=$1
 instance_count=$2
 instance_end=$instance_index+$instance_count
 
-seed='172.16.55.229'
-seedId='Qmcf3W4uSAaAttmYCG7ZnCXoP6eytmka9Q1po6ArgBr5XF'
-
 for((;instance_index<instance_end;instance_index++))
 
 do
@@ -33,11 +30,11 @@ do
 	fi
 
 	if [ $instance_index -eq 1 ];then
-		nohup ./gx miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --pprof $pprof_port  --seed $seed --seedId $seedId --apply light --keystore keystore$instance_index > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gx miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --pprof $pprof_port  --apply light --keystore keystore$instance_index > $stdout_log 2>&1 & echo $! > $pid_file
     elif [ $instance_index -le 6 ];then
-		nohup ./gx miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --seed $seed  --seedId $seedId --apply light --keystore keystore$instance_index > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gx miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index > $stdout_log 2>&1 & echo $! > $pid_file
 	else
-		nohup ./gx miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port --seed $seed --seedId $seedId --apply heavy --keystore keystore$instance_index > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gx miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port --apply heavy --keystore keystore$instance_index > $stdout_log 2>&1 & echo $! > $pid_file
 	fi
 	sleep 1
 done
