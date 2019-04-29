6c9b346c5e1a40ff726b263e7bcf30fbdbf9c5a9
小熊 <1581487855@qq.com>
2019-04-29 16:36:00 +0800

    stop docker while stop program

--- src/middleware/types/transacton_test.go
+++ src/middleware/types/transacton_test.go
@@ -16,39 +16,28 @@ func TestAssetOnChainTransactionHash(t *testing.T) {
 	str := string(b)
 
 	txJson := TxJson{Source: "dragonMother", Target: "tuntunbiu", Type: 202, Data: str, Nonce: 2, Time: "1556076659050692000"}
-	j, _ := json.Marshal(txJson)
-	fmt.Printf("json:%s\n", string(j))
-
 	tx := txJson.ToTransaction()
-	fmt.Printf("TX:%v\n", tx)
+	tx.Hash = tx.GenHash()
 
-	hash := tx.GenHash().String()
-	fmt.Printf("Hash:%s\n", hash)
+	j,_:=  json.Marshal(tx.ToTxJson())
+	fmt.Printf("TX JSON:%s\n", string(j))
 
 }
 
 func TestUpgradeDragonTransactionHash(t *testing.T) {
 	txJson := TxJson{Target: "tuntunbiu", Type: 8, Data: "u", Nonce: 2,}
-	j, _ := json.Marshal(txJson)
-	fmt.Printf("json:%s\n", string(j))
-
 	tx := txJson.ToTransaction()
-	fmt.Printf("TX:%v\n", tx)
+	tx.Hash = tx.GenHash()
 
-	hash := tx.GenHash().String()
-	fmt.Printf("Hash:%s\n", hash)
-
+	j,_:=  json.Marshal(tx.ToTxJson())
+	fmt.Printf("TX JSON:%s\n", string(j))
 }
 
 func TestWithdrawTransactionHash(t *testing.T) {
 	txJson := TxJson{Source: "dragonMother", Target: "tuntunbiu", Type: 201, Data: "1.2", Nonce: 1, Time: "1556076659050692000"}
-	j, _ := json.Marshal(txJson)
-	fmt.Printf("json:%s\n", string(j))
-
 	tx := txJson.ToTransaction()
-	fmt.Printf("TX:%v\n", tx)
-
 	tx.Hash = tx.GenHash()
-	fmt.Printf("Hash:%s\n", tx.Hash.String())
 
+	j,_:=  json.Marshal(tx.ToTxJson())
+	fmt.Printf("TX JSON:%s\n", string(j))
 }
