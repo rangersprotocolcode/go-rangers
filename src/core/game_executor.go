29dcc7affe023326d2dcc5d39f0bb12ec638f89f
小熊 <1581487855@qq.com>
2019-07-09 15:30:01 +0800

    bug fix

--- src/core/game_executor.go
+++ src/core/game_executor.go
@@ -128,9 +128,10 @@ func (executor *GameExecutor) Read(msg notify.Message) {
 
 func (executor *GameExecutor) makeSuccessResponse(bytes string, hash string) []byte {
 	res := response{
-		Data:   bytes,
-		Hash:   hash,
-		Status: 0,
+		Data:    bytes,
+		Hash:    hash,
+		Status:  0,
+		Version: "0.1",
 	}
 
 	logger.Debugf("res:%v", res)
@@ -238,7 +239,7 @@ func (executor *GameExecutor) runTransaction(txRaw types.Transaction) string {
 			// 调用状态机
 			outputMessage := statemachine.Docker.Process(txRaw.Target, "operator", strconv.FormatUint(txRaw.Nonce, 10), txRaw.Data)
 			logger.Infof("invoke state machine result:%v", outputMessage)
-			if outputMessage != nil{
+			if outputMessage != nil {
 				result = outputMessage.Payload
 			}
 		}
