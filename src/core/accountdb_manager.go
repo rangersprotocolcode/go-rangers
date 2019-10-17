3e2762b47b2f004f4472eb6c2a6c4df4836e636d
tequila <114778836@qq.com>
2019-10-17 13:41:16 +0800

    change accountDB manager

--- src/core/accountdb_manager.go
+++ src/core/accountdb_manager.go
@@ -6,19 +6,9 @@ import (
 	"x/src/storage/account"
 )
 
-const BASE = "base"
-
-// 基于gameId的accountdb管理器
-// 用于实时计算
 type AccountDBManager struct {
-	context map[string]*accountContext
-	lock    *sync.RWMutex //用于锁定BASE
-}
-
-type accountContext struct {
 	accountDB *account.AccountDB
-	height    uint64
-	lock      *sync.Mutex // 锁自己
+	lock      *sync.RWMutex //用于锁定BASE
 }
 
 var AccountDBManagerInstance *AccountDBManager
@@ -26,22 +16,19 @@ var AccountDBManagerInstance *AccountDBManager
 func initAccountDBManager() {
 	AccountDBManagerInstance = &AccountDBManager{}
 	AccountDBManagerInstance.lock = &sync.RWMutex{}
-	AccountDBManagerInstance.context = make(map[string]*accountContext)
 
 	// chain初始化在前，所以链上数据已经有了
-	//AccountDBManagerInstance.onBlockAddSuccess(nil)
-	//notify.BUS.Subscribe(notify.BlockAddSucc, AccountDBManagerInstance.onBlockAddSuccess)
+	AccountDBManagerInstance.onBlockAddSuccess(nil)
+	notify.BUS.Subscribe(notify.BlockAddSucc, AccountDBManagerInstance.onBlockAddSuccess)
 }
 
 //事件onBlockAddSuccess，表示着上链已经完成，链上已经是最新状态了
 func (manager *AccountDBManager) onBlockAddSuccess(message notify.Message) {
-	context := &accountContext{height: GetBlockChain().Height(), accountDB: GetBlockChain().GetAccountDB()}
-
 	manager.lock.Lock()
 	defer manager.lock.Unlock()
-	manager.context[BASE] = context
+	manager.accountDB = GetBlockChain().GetAccountDB()
 }
 
 func (manager *AccountDBManager) GetAccountDB(gameId string, isBase bool) *account.AccountDB {
-	return blockChainImpl.GetAccountDB()
+	return manager.accountDB
 }
