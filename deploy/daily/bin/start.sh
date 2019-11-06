11579ae3951872cfa44cf955eb9215e98162d28e
tequila <114778836@qq.com>
2019-11-06 10:37:05 +0800

    add gotraceback

--- deploy/daily/bin/start.sh
+++ deploy/daily/bin/start.sh
@@ -31,11 +31,11 @@ do
 	fi
 
 	if [ $instance_index -eq 1 ];then
-		nohup ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index --env daily > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index --env daily > $stdout_log 2>&1 & echo $! > $pid_file
     elif [ $instance_index -le 3 ];then
-		nohup ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index --env daily > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index --env daily > $stdout_log 2>&1 & echo $! > $pid_file
 	else
-		nohup ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --apply heavy --keystore keystore$instance_index --env daily > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --apply heavy --keystore keystore$instance_index --env daily > $stdout_log 2>&1 & echo $! > $pid_file
 	fi
 	sleep 1
 done
