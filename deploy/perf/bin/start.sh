1acefc4b44528d83dfa4bfad86075857946f61f5
tequila <114778836@qq.com>
2019-11-20 11:31:52 +0800

    use production gateway

--- deploy/perf/bin/start.sh
+++ deploy/perf/bin/start.sh
@@ -31,11 +31,11 @@ do
 	fi
 
 	if [ $instance_index -eq 1 ];then
-		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index  > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --env production --apply light --keystore keystore$instance_index  > $stdout_log 2>&1 & echo $! > $pid_file
     elif [ $instance_index -le 3 ];then
-		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port   --apply light --keystore keystore$instance_index  > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --env production --apply light --keystore keystore$instance_index  > $stdout_log 2>&1 & echo $! > $pid_file
 	else
-		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --apply heavy --keystore keystore$instance_index   > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup env GOTRACEBACK=crash ./rocket_node miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port  --env production --apply heavy --keystore keystore$instance_index   > $stdout_log 2>&1 & echo $! > $pid_file
 	fi
 	sleep 1
 done
