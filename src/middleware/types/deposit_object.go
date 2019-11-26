f526e08be8a4c073cd0ea7ee8a815b63adac9032
小熊 <1581487855@qq.com>
2019-11-26 11:30:47 +0800

    deposit data add mainchain address and contract address

--- src/middleware/types/deposit_object.go
+++ src/middleware/types/deposit_object.go
@@ -2,16 +2,19 @@ package types
 
 // 主链币
 type DepositCoinData struct {
-	ChainType string `json:"chainType,omitempty"`
-	Amount    string `json:"amount,omitempty"`
-	TxId      string `json:"txId,omitempty"`
+	ChainType        string `json:"chainType,omitempty"`
+	Amount           string `json:"amount,omitempty"`
+	TxId             string `json:"txId,omitempty"`
+	MainChainAddress string `json:"Addr,omitempty"`
 }
 
 //FT充值确认数据结构
 type DepositFTData struct {
-	FTId   string `json:"ftId,omitempty"`
-	Amount string `json:"amount,omitempty"`
-	TxId   string `json:"txId,omitempty"`
+	FTId             string `json:"ftId,omitempty"`
+	Amount           string `json:"amount,omitempty"`
+	MainChainAddress string `json:"Addr,omitempty"`
+	ContractAddress  string `json:"ContractAddr,omitempty"`
+	TxId             string `json:"txId,omitempty"`
 }
 
 //NFT充值确认数据结构
@@ -28,11 +31,13 @@ type DepositNFTData struct {
 	Condition  byte              `json:"condition,omitempty"`
 	AppId      string            `json:"appId,omitempty"`
 	Data       map[string]string `json:"data,omitempty"`
-	TxId       string            `json:"txId,omitempty"`
+
+	MainChainAddress string `json:"Addr,omitempty"`
+	ContractAddress  string `json:"ContractAddr,omitempty"`
+	TxId             string `json:"txId,omitempty"`
 }
 
-
 type Deposit struct {
-	Method string  `json:"type,omitempty"`
-	Data map[string]string `json:"data,omitempty"`
+	Method string            `json:"type,omitempty"`
+	Data   map[string]string `json:"data,omitempty"`
 }
