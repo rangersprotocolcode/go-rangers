eaf4a79aa6029c59f4c4d0478eb2ba1385fe0ecf
小熊 <1581487855@qq.com>
2020-09-01 14:24:37 +0800

    start.sh env change to production

--- src/common/publickey_test.go
+++ src/common/publickey_test.go
@@ -17,8 +17,10 @@
 package common
 
 import (
+	"encoding/hex"
 	"fmt"
 	"golang.org/x/crypto/sha3"
+	"math/big"
 	"testing"
 )
 
@@ -31,8 +33,17 @@ func TestSha256(t *testing.T) {
 }
 
 func TestID(t *testing.T) {
-	pubkey := HexStringToPubKey("0x04084b31e8fe11bf53acbabb035c31b5f7af35e78441b48b4b749cc588a560a67fe0b3909dee8b5302dcab0e1ad0af5bcc2bc360f2add174bfa3e5b9d39bf319f5")
-	fmt.Printf(pubkey.GetAddress().String())
-}
+	privateKeyStr := "0xe7260a418579c2e6ca36db4fe0bf70f84d687bdf7ec6c0c181b43ee096a84aea  "
+	privateKeyBuf, _ := hex.DecodeString(privateKeyStr[len(PREFIX):])
+	fmt.Printf("privateKeyBuf len:%d\n", len(privateKeyBuf))
+	var privateKey PrivateKey
+	privateKey.PrivKey.PublicKey.Curve = getDefaultCurve()
+	privateKey.PrivKey.D = new(big.Int).SetBytes(privateKeyBuf)
+	privateKey.PrivKey.PublicKey.X, privateKey.PrivKey.PublicKey.Y = getDefaultCurve().ScalarBaseMult(privateKey.PrivKey.D.Bytes())
 
-//privateKey:b14bd529fd9707271d31d4ff6329d556186a3000ae30a75015d92392bcd546d9|publicKey:0x04bdc3aff18404ed172aa660a8fcace24362be6f2376b2ee3e98447084e200e79419e555a8e13b64d1ed3711b18458aa931ce19b614f9ec60370ff9978eaf904eb|message:92767d34410fe5e8b450be0a6b2776cef82ce47887df274214679893099ccfee|sign:0xcfeaebea4bd8891e071b13cc0f23d674db5786c7d758f7440e33af26f831146f117b93839a6bfb26406cf445c71061dae7f2472f51a6e67fd949cd745057db561c|id:0x60f686a8f4817675c783bb38be2975773f082a4e2291b5bfc36281deda956616
+	pubkey := privateKey.GetPubKey()
+	address := pubkey.GetAddress()
+	fmt.Printf("SK:%v\n", privateKey.GetHexString())
+	fmt.Printf("pubkey:%v\n", pubkey.GetHexString())
+	fmt.Printf("address:%v\n", address.GetHexString())
+}
