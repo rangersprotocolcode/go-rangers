f8620763e2ae84da911f05d4820d9ea0914df23a
小熊 <1581487855@qq.com>
2020-09-02 15:15:14 +0800

    change dir deploy

--- deploy/perf/bin/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/testx/run
-for file in pid/pid_gx*
-do
-    kill -9 `cat $file`
-    rm -f $file
-done
---
+++ deploy/test/bin/clear.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf /home/rocket_node_test/run/d*
+rm -rf /home/rocket_node_test/run/jgs*
+rm -rf /home/rocket_node_test/run/logs
+rm -rf /home/rocket_node_test/run/pid
