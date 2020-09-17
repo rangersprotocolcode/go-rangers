7aae59a3025f4d7151ff90e03ee9c35dd3e52b6e
tequila <114778836@qq.com>
2020-09-17 15:51:16 +0800

    change storage path

--- deploy/dev/bin/clear.sh
+++ deploy/dev/bin/clear.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf /home/rocket_node_dev/run/d*
-rm -rf /home/rocket_node_dev/run/jgs*
+rm -rf /home/rocket_node_dev/run/storage*
 rm -rf /home/rocket_node_dev/run/logs
 rm -rf /home/rocket_node_dev/run/pid
--- deploy/test/bin/clear.sh
+++ deploy/test/bin/clear.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf /home/rocket_node_test/run/d*
-rm -rf /home/rocket_node_test/run/jgs*
+rm -rf /home/rocket_node_test/run/storage*
 rm -rf /home/rocket_node_test/run/logs
 rm -rf /home/rocket_node_test/run/pid
