6fea976c0dc5576517ea9e65be1425f4e1c55526
小熊 <1581487855@qq.com>
2020-11-27 14:25:17 +0800

    change vm log

--- src/vm/logger_test.go
+++ src/vm/logger_test.go
@@ -16,55 +16,55 @@
 
 package vm
 
-//
-//import (
-//	"com.tuntun.rocket/node/src/common"
-//	"math/big"
-//	"testing"
-//	"github.com/holiman/uint256"
-//)
-//
-//type dummyContractRef struct {
-//	calledForEach bool
-//}
-//
-//func (dummyContractRef) ReturnGas(*big.Int)          {}
-//func (dummyContractRef) Address() common.Address     { return common.Address{} }
-//func (dummyContractRef) Value() *big.Int             { return new(big.Int) }
-//func (dummyContractRef) SetCode(common.Hash, []byte) {}
-//func (d *dummyContractRef) ForEachStorage(callback func(key, value common.Hash) bool) {
-//	d.calledForEach = true
-//}
-//func (d *dummyContractRef) SubBalance(amount *big.Int) {}
-//func (d *dummyContractRef) AddBalance(amount *big.Int) {}
-//func (d *dummyContractRef) SetBalance(*big.Int)        {}
-//func (d *dummyContractRef) SetNonce(uint64)            {}
-//func (d *dummyContractRef) Balance() *big.Int          { return new(big.Int) }
-//
-//type dummyStatedb struct {
-//	state.StateDB
-//}
-//
-//func (*dummyStatedb) GetRefund() uint64 { return 1337 }
-//
-//func TestStoreCapture(t *testing.T) {
-//	var (
-//		env      = NewEVM(Context{}, &dummyStatedb{}, TestChainConfig, Config{})
-//		logger   = NewStructLogger(nil)
-//		mem      = NewMemory()
-//		stack    = newstack()
-//		rstack   = newReturnStack()
-//		contract = NewContract(&dummyContractRef{}, &dummyContractRef{}, new(big.Int), 0)
-//	)
-//	stack.push(uint256.NewInt().SetUint64(1))
-//	stack.push(uint256.NewInt())
-//	var index common.Hash
-//	logger.CaptureState(env, 0, SSTORE, 0, 0, mem, stack, rstack, nil, contract, 0, nil)
-//	if len(logger.storage[contract.Address()]) == 0 {
-//		t.Fatalf("expected exactly 1 changed value on address %x, got %d", contract.Address(), len(logger.storage[contract.Address()]))
-//	}
-//	exp := common.BigToHash(big.NewInt(1))
-//	if logger.storage[contract.Address()][index] != exp {
-//		t.Errorf("expected %x, got %x", exp, logger.storage[contract.Address()][index])
-//	}
-//}
+import (
+	"com.tuntun.rocket/node/src/common"
+	"com.tuntun.rocket/node/src/storage/account"
+	"github.com/holiman/uint256"
+	"math/big"
+	"testing"
+)
+
+type dummyContractRef struct {
+	calledForEach bool
+}
+
+func (dummyContractRef) ReturnGas(*big.Int)          {}
+func (dummyContractRef) Address() common.Address     { return common.Address{} }
+func (dummyContractRef) Value() *big.Int             { return new(big.Int) }
+func (dummyContractRef) SetCode(common.Hash, []byte) {}
+func (d *dummyContractRef) ForEachStorage(callback func(key, value common.Hash) bool) {
+	d.calledForEach = true
+}
+func (d *dummyContractRef) SubBalance(amount *big.Int) {}
+func (d *dummyContractRef) AddBalance(amount *big.Int) {}
+func (d *dummyContractRef) SetBalance(*big.Int)        {}
+func (d *dummyContractRef) SetNonce(uint64)            {}
+func (d *dummyContractRef) Balance() *big.Int          { return new(big.Int) }
+
+type dummyStatedb struct {
+	account.AccountDB
+}
+
+func (*dummyStatedb) GetRefund() uint64 { return 1337 }
+
+func TestStoreCapture(t *testing.T) {
+	var (
+		env      = NewEVM(Context{}, &dummyStatedb{}, testChainID, Config{})
+		logger   = NewStructLogger(nil)
+		mem      = NewMemory()
+		stack    = newstack()
+		rstack   = newReturnStack()
+		contract = NewContract(&dummyContractRef{}, &dummyContractRef{}, new(big.Int), 0)
+	)
+	stack.push(uint256.NewInt().SetUint64(1))
+	stack.push(uint256.NewInt())
+	var index common.Hash
+	logger.CaptureState(env, 0, SSTORE, 0, 0, mem, stack, rstack, nil, contract, 0, nil)
+	if len(logger.storage[contract.Address()]) == 0 {
+		t.Fatalf("expected exactly 1 changed value on address %x, got %d", contract.Address(), len(logger.storage[contract.Address()]))
+	}
+	exp := common.BigToHash(big.NewInt(1))
+	if logger.storage[contract.Address()][index] != exp {
+		t.Errorf("expected %x, got %x", exp, logger.storage[contract.Address()][index])
+	}
+}
