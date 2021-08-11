5f8eb13adfb12461003ee21eaa2039bdc3cae233
littlebear <1581487855@qq.com>
2021-08-11 17:24:07 +0800

    add genesis proposer

--- src/core/blockchain_genesis.go
+++ src/core/blockchain_genesis.go
@@ -84,9 +84,9 @@ func genGenesisBlock(stateDB *account.AccountDB, triedb *trie.NodeDatabase, gene
 	block.Header.Signature = common.Sha256([]byte("tuntunhz"))
 	block.Header.Random = common.Sha256([]byte("RocketProtocolVRF"))
 
-	//genesisProposers := getGenesisProposer()
-	//addMiners(genesisProposers, stateDB)
-	//
+	genesisProposers := getGenesisProposer()
+	addMiners(genesisProposers, stateDB)
+
 	//verifyMiners := make([]*types.Miner, 0)
 	//for _, genesis := range genesisInfo {
 	//	for i, member := range genesis.Group.Members {
