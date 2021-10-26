a1cbcf2ef2fb7108a18d989f91a6121ab0791c2b
littlebear <1581487855@qq.com>
2021-10-26 17:28:16 +0800

    print env

--- src/gx/cli/gx.go
+++ src/gx/cli/gx.go
@@ -115,7 +115,7 @@ func (gx *GX) Run() {
 
 	walletManager = newWallets()
 	fmt.Println("Welcome to be a RangersProtocol miner!")
-	fmt.Printf("Chain ID:%s,Network ID:%s\n", common.ChainId, common.NetworkId)
+	fmt.Printf("Env:%s,Chain ID:%s,Network ID:%s\n", *env, common.ChainId, common.NetworkId)
 	switch command {
 	case versionCmd.FullCommand():
 		fmt.Println("GX Version:", GXVersion)
