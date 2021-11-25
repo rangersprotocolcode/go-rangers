cff4bff1bf9d5b9ac70fec75f4066f50ed746dfb
littlebear <1581487855@qq.com>
2021-11-25 10:20:14 +0800

    gx.go :use common.ChainId() instead of common.ChainId

--- src/gx/cli/gx.go
+++ src/gx/cli/gx.go
@@ -216,7 +216,7 @@ func syncChainInfo(privateKey common.PrivateKey, id string) {
 			}
 			topBlock := core.GetBlockChain().TopBlock()
 			jsonObject := types.NewJSONObject()
-			jsonObject.Put("chainId", common.ChainId)
+			jsonObject.Put("chainId", common.ChainId())
 			jsonObject.Put("instanceNum", common.GlobalConf.GetInt(instanceSection, indexKey, 0))
 			jsonObject.Put("candidateHeight", candidateHeight)
 			if topBlock != nil {
