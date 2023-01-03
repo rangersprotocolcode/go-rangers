acd23c77ad4b7e5642323558c6da5eecfca85dc0
tequila <114778836@qq.com>
2023-01-03 16:37:59 +0800

    eip3074 testcase

--- src/vm/instructions_test.go
+++ src/vm/instructions_test.go
@@ -103,7 +103,7 @@ func testTwoOperandOp(t *testing.T, tests []TwoOperandTestcase, opFn executionFu
 		expected := new(uint256.Int).SetBytes(common.Hex2Bytes(test.Expected))
 		stack.push(x)
 		stack.push(y)
-		opFn(&pc, evmInterpreter, &callCtx{nil, stack, rstack, nil, make([]*types.Log, 0)})
+		opFn(&pc, evmInterpreter, &callCtx{nil, stack, rstack, nil, nil, make([]*types.Log, 0)})
 		if len(stack.data) != 1 {
 			t.Errorf("Expected one item on stack after %v, got %d: ", name, len(stack.data))
 		}
@@ -218,7 +218,7 @@ func TestAddMod(t *testing.T) {
 		stack.push(z)
 		stack.push(y)
 		stack.push(x)
-		opAddmod(&pc, evmInterpreter, &callCtx{nil, stack, nil, nil, make([]*types.Log, 0)})
+		opAddmod(&pc, evmInterpreter, &callCtx{nil, stack, nil, nil, nil, make([]*types.Log, 0)})
 		actual := stack.pop()
 		if actual.Cmp(expected) != 0 {
 			t.Errorf("Testcase %d, expected  %x, got %x", i, expected, actual)
@@ -240,7 +240,7 @@ func getResult(args []*twoOperandParams, opFn executionFunc) []TwoOperandTestcas
 		y := new(uint256.Int).SetBytes(common.Hex2Bytes(param.y))
 		stack.push(x)
 		stack.push(y)
-		opFn(&pc, interpreter, &callCtx{nil, stack, rstack, nil, make([]*types.Log, 0)})
+		opFn(&pc, interpreter, &callCtx{nil, stack, rstack, nil, nil, make([]*types.Log, 0)})
 		actual := stack.pop()
 		result[i] = TwoOperandTestcase{param.x, param.y, fmt.Sprintf("%064x", actual)}
 	}
@@ -290,12 +290,12 @@ func TestOpMstore(t *testing.T) {
 	pc := uint64(0)
 	v := "abcdef00000000000000abba000000000deaf000000c0de00100000000133700"
 	stack.pushN(*new(uint256.Int).SetBytes(common.Hex2Bytes(v)), *new(uint256.Int))
-	opMstore(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, make([]*types.Log, 0)})
+	opMstore(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, nil, make([]*types.Log, 0)})
 	if got := common.Bytes2Hex(mem.GetCopy(0, 32)); got != v {
 		t.Fatalf("Mstore fail, got %v, expected %v", got, v)
 	}
 	stack.pushN(*new(uint256.Int).SetUint64(0x1), *new(uint256.Int))
-	opMstore(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, make([]*types.Log, 0)})
+	opMstore(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, nil, make([]*types.Log, 0)})
 	if common.Bytes2Hex(mem.GetCopy(0, 32)) != "0000000000000000000000000000000000000000000000000000000000000001" {
 		t.Fatalf("Mstore failed to overwrite previous value")
 	}
@@ -322,7 +322,7 @@ func opBenchmark(bench *testing.B, op executionFunc, args ...string) {
 			a.SetBytes(arg)
 			stack.push(a)
 		}
-		op(&pc, evmInterpreter, &callCtx{nil, stack, rstack, nil, make([]*types.Log, 0)})
+		op(&pc, evmInterpreter, &callCtx{nil, stack, rstack, nil, nil, make([]*types.Log, 0)})
 		stack.pop()
 	}
 }
@@ -552,7 +552,7 @@ func BenchmarkOpMstore(bench *testing.B) {
 	bench.ResetTimer()
 	for i := 0; i < bench.N; i++ {
 		stack.pushN(*value, *memStart)
-		opMstore(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, make([]*types.Log, 0)})
+		opMstore(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, nil, make([]*types.Log, 0)})
 	}
 }
 
@@ -571,6 +571,6 @@ func BenchmarkOpSHA3(bench *testing.B) {
 	bench.ResetTimer()
 	for i := 0; i < bench.N; i++ {
 		stack.pushN(*uint256.NewInt().SetUint64(32), *start)
-		opSha3(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, make([]*types.Log, 0)})
+		opSha3(&pc, evmInterpreter, &callCtx{mem, stack, rstack, nil, nil, make([]*types.Log, 0)})
 	}
 }
