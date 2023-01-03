134fca6934c8d48373d537fbd4c0e3fbba3f2e91
tequila <114778836@qq.com>
2023-01-03 16:28:57 +0800

    GenesisConf

--- src/common/genesis_conf.go
+++ src/common/genesis_conf.go
@@ -1,16 +1,42 @@
 package common
+//
+//type GenesisConf struct {
+//	ChainId string `json:"chainId"`
+//	Name    string `json:"name"`
+//
+//	Cast      uint64 `json:"cast"`
+//	GroupLife uint64 `json:"groupLife"`
+//
+//	GenesisTime int64 `json:"genesisTime"`
+//
+//	Group       string `json:"group"`
+//	JoinedGroup string `json:"joined"`
+//
+//	ProposerInfo []string `json:"proposers"`
+//}
+
 
 type GenesisConf struct {
 	ChainId string `json:"chainId"`
 	Name    string `json:"name"`
 
-	Cast      uint64 `json:"cast"`
-	GroupLife uint64 `json:"groupLife"`
+	Cast       uint64 `json:"cast"`
+	GroupLife  uint64 `json:"groupLife"`
+	Proposals  uint64 `json:"p"`
+	Validators uint64 `json:"v"`
 
 	GenesisTime int64 `json:"genesisTime"`
 
-	Group       string `json:"group"`
-	JoinedGroup string `json:"joined"`
+	TimeCycle      int    `json:"timecycle"`
+	TokenName      string `json:"tokenName"`
+	TotalSupply    uint64 `json:"totalsupply"`
+	Symbol         string `json:"symbol"`
+	Decimal        int    `json:"decimal"`
+	ProposalToken  int    `json:"ptoken"`
+	ValidatorToken int    `json:"vtoken"`
 
+	// 生成的创始组与创始矿工
+	Group        string   `json:"group"`
+	JoinedGroup  string   `json:"joined"`
 	ProposerInfo []string `json:"proposers"`
 }
