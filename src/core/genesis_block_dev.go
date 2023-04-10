aaaf9cd37a72c33f6326fb4c99536d2b4aaee50b
LittleBear <1581487855@qq.com>
2023-04-10 14:52:08 +0800

    change dev genesis block time

--- src/core/genesis_block_dev.go
+++ src/core/genesis_block_dev.go
@@ -71,7 +71,7 @@ func genDevGenesisBlock(stateDB *account.AccountDB, triedb *trie.NodeDatabase, g
 	block.Header = &types.BlockHeader{
 		Height:       0,
 		ExtraData:    common.Sha256([]byte("Rangers Protocol")),
-		CurTime:      time.Date(2022, 12, 15, 0, 0, 0, 0, time.UTC),
+		CurTime:      time.Date(2023, 04, 10, 0, 0, 0, 0, time.UTC),
 		ProveValue:   pv,
 		TotalQN:      0,
 		Transactions: make([]common.Hashes, 0), //important!!
