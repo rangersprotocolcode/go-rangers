1802fd62a38a155e7c397348d3f74a3160481e18
tequila <114778836@qq.com>
2023-06-13 10:04:26 +0800

    change name

--- src/consensus/logical/processor_misc.go
+++ src/consensus/logical/processor_misc.go
@@ -1,12 +1,12 @@
-// Copyright 2020 The RocketProtocol Authors
+// Copyright 2020 The RangersProtocol Authors
 // This file is part of the RocketProtocol library.
 //
-// The RocketProtocol library is free software: you can redistribute it and/or modify
+// The RangersProtocol library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The RocketProtocol library is distributed in the hope that it will be useful,
+// The RangersProtocol library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
@@ -25,7 +25,7 @@ import (
 	"fmt"
 )
 
-//后续如有全局定时器，从这个函数启动
+// 后续如有全局定时器，从这个函数启动
 func (p *Processor) Start() bool {
 	// 检查是否要出块
 	p.Ticker.RegisterRoutine(p.getCastCheckRoutineName(), p.checkSelfCastRoutine, common.CastingCheckInterval)
@@ -47,7 +47,7 @@ func (p *Processor) Start() bool {
 	return true
 }
 
-//预留接口
+// 预留接口
 func (p *Processor) Stop() {
 	return
 }
