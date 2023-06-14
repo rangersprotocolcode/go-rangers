ab6763da23169bdc79efb83ebbfa110052407c11
tequila <114778836@qq.com>
2023-06-14 15:08:29 +0800

    format comments

--- src/gx/cli/errors.go
+++ src/gx/cli/errors.go
@@ -1,12 +1,12 @@
-// Copyright 2020 The RocketProtocol Authors
+// Copyright 2020 The RangersProtocol Authors
 // This file is part of the RocketProtocol library.
 //
-// The RocketProtocol library is free software: you can redistribute it and/or modify
+// The RangersProtocol library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The RocketProtocol library is distributed in the hope that it will be useful,
+// The RangersProtocol library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
@@ -19,11 +19,11 @@ package cli
 import "errors"
 
 var (
-	// ErrorBlockChainUninitialized 未初始化链
+	// ErrorBlockChainUninitialized
 	ErrorBlockChainUninitialized = errors.New("should init blockchain module first")
-	// ErrorP2PUninitialized 未初始化P2P模块。
+	// ErrorP2PUninitialized
 	ErrorP2PUninitialized = errors.New("should init P2P module first")
-	// ErrorGovUninitialized 未初始化共识模块。
+	// ErrorGovUninitialized
 	ErrorGovUninitialized = errors.New("should init Governance module first")
 	// ErrorWalletsUninitialized
 	ErrorWalletsUninitialized = errors.New("should load wallets from config")
--- src/gx/cli/wallets.go
+++ src/gx/cli/wallets.go
@@ -1,12 +1,12 @@
-// Copyright 2020 The RocketProtocol Authors
+// Copyright 2020 The RangersProtocol Authors
 // This file is part of the RocketProtocol library.
 //
-// The RocketProtocol library is free software: you can redistribute it and/or modify
+// The RangersProtocol library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The RocketProtocol library is distributed in the hope that it will be useful,
+// The RangersProtocol library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
@@ -38,12 +38,11 @@ type wallet struct {
 	Address    string `json:"address"`
 }
 
-//存储钱包账户
 func (ws *wallets) store() {
 	js, err := json.Marshal(ws)
 	if err != nil {
 		log.Println("store wallets error")
-		// TODO 输出log
+		return
 	}
 	common.GlobalConf.SetString(Section, "wallets", string(js))
 }
@@ -60,7 +59,6 @@ func (ws *wallets) deleteWallet(key string) {
 	ws.store()
 }
 
-// newWallet 新建钱包并存储到config文件中
 func (ws *wallets) newWallet() (privKeyStr, walletAddress, minerString string) {
 	mutex.Lock()
 	defer mutex.Unlock()
