d545719b7a37368c888d15e6fcf532d43863698f
tequila <114778836@qq.com>
2023-06-14 13:48:04 +0800

    add gnosis contracts

--- src/executor/contract_executor.go
+++ src/executor/contract_executor.go
@@ -91,13 +91,10 @@ func (this *contractExecutor) Execute(transaction *types.Transaction, header *ty
 		contractAddress = common.HexToAddress(transaction.Target)
 	)
 
-	nonce1 := accountdb.GetNonce(caller.Address())
-
 	if transaction.Target == "" {
 		result, contractAddress, leftOverGas, logs, err = vmInstance.Create(caller, input, vmCtx.GasLimit, transferValue)
 		context["contractAddress"] = contractAddress
 
-		this.logger.Tracef("hash: %s, source: %s, target: %s, data: %s, nonce: %d, nonce: %d, contract: %s", transaction.Hash.String(), vmCtx.Origin.String(), transaction.Target, transaction.Data, nonce1, accountdb.GetNonce(caller.Address()), contractAddress.String())
 		this.logger.Tracef("After execute contract create!Contract address:%s, leftOverGas: %d,error:%v", contractAddress.GetHexString(), leftOverGas, err)
 	} else {
 		if common.IsProposal007() {
@@ -106,13 +103,14 @@ func (this *contractExecutor) Execute(transaction *types.Transaction, header *ty
 		}
 		result, leftOverGas, logs, err = vmInstance.Call(caller, contractAddress, input, vmCtx.GasLimit, transferValue)
 
-		this.logger.Tracef("hash: %s, source: %s, target: %s, data: %s, nonce: %d, nonce: %d", transaction.Hash.String(), vmCtx.Origin.String(), transaction.Target, transaction.Data, nonce1, accountdb.GetNonce(caller.Address()))
 		this.logger.Tracef("After execute contract call! result:%v,leftOverGas: %d,error:%v", result, leftOverGas, err)
 	}
+
 	context["logs"] = logs
 	if err != nil {
 		return false, err.Error()
 	}
+
 	returnData := executeResultData{contractAddress.GetHexString(), toHex(result), logs}
 	json, _ := json.Marshal(returnData)
 	return true, string(json)
