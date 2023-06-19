96671bafc13db25ef311cc7f9fa272e58109e24e
LittleBear <1581487855@qq.com>
2023-06-19 16:49:18 +0800

    ethereum adapt:jsonrpc method err return add failed reason

--- src/eth_rpc/response.go
+++ src/eth_rpc/response.go
@@ -43,7 +43,7 @@ type jsonError struct {
 type jsonErrResponse struct {
 	Version string      `json:"jsonrpc"`
 	Id      interface{} `json:"id,omitempty"`
-	Error   jsonError   `json:"result"`
+	Error   jsonError   `json:"error"`
 }
 
 func (r jsonSuccessResponse) encodeJson() ([]byte, error) {
