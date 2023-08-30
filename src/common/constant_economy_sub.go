0f241591b0e82cb2fe92ac1f7f8464a84c2b8786
tequila <114778836@qq.com>
2023-08-30 14:10:52 +0800

    change contract

--- src/common/constant_economy_sub.go
+++ src/common/constant_economy_sub.go
@@ -5,11 +5,11 @@ import "com.tuntun.rocket/node/src/utility"
 var (
 	EconomyContract = HexToAddress("0x71d9cfd1b7adb1e8eb4c193ce6ffbe19b4aee0db")
 
-	ProxySubGovernance = HexToAddress("0xe3e5df5d1ea7e2a7e107e35098412aa6016bf89c")
+	ProxySubGovernance = HexToAddress("0x57d9b509004657dfade3121d9aa32a3586c1ef49")
 
 	WhitelistForCreate = "0x9c1cbfe5328dfb1733d59a7652d0a49228c7e12c"
 
-	RpgReward = HexToAddress("0xcde68d63304537f693214b0ab730b99eda6281a9")
+	RpgReward = HexToAddress("0x4e0285462d9592fdcef54c9b0f8435814096c299")
 
 	CreateWhiteListAddr = HexToAddress(WhitelistForCreate)
 
