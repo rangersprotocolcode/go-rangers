031e2ff51fce00b7f5b86c0abcc5132d9e5b7471
tequila <114778836@qq.com>
2023-09-20 17:02:12 +0800

    change contract

--- src/core/genesis_sub_test.go
+++ src/core/genesis_sub_test.go
@@ -33,41 +33,6 @@ import (
 	"time"
 )
 
-func TestGnosisContract(t *testing.T) {
-	defer func() {
-		os.RemoveAll("storage0")
-		os.RemoveAll("logs")
-		os.RemoveAll("1.ini")
-	}()
-
-	common.Init(0, "1.ini", "dev")
-	vm.InitVM()
-
-	block := new(types.Block)
-	pv := big.NewInt(0)
-	block.Header = &types.BlockHeader{
-		Height:       0,
-		ProveValue:   pv,
-		TotalQN:      0,
-		Transactions: make([]common.Hashes, 0), //important!!
-		EvictedTxs:   make([]common.Hash, 0),   //important!!
-		Nonce:        ChainDataVersion,
-	}
-
-	block.Header.RequestIds = make(map[string]uint64)
-
-	db, err := db.NewLDBDatabase("state", 128, 2048)
-	if err != nil {
-		t.Fatal(err)
-	}
-	stateDB, err := account.NewAccountDB(common.Hash{}, account.NewDatabase(db))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	createGnosisContract(block.Header, stateDB)
-}
-
 func TestCreateSubCrossContract(t *testing.T) {
 	defer func() {
 		os.RemoveAll("storage0")
