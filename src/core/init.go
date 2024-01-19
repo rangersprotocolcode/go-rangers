5686463d3c94831f48694fa344df6308c1280da7
LittleBear <1581487855@qq.com>
2024-01-20 02:37:24 +0800

    missing tx

--- src/core/init.go
+++ src/core/init.go
@@ -22,6 +22,7 @@ import (
 	"com.tuntun.rangers/node/src/middleware/log"
 	"com.tuntun.rangers/node/src/middleware/types"
 	"com.tuntun.rangers/node/src/service"
+	"math/big"
 	"strconv"
 	"time"
 )
@@ -73,6 +74,8 @@ func InitCore(helper types.ConsensusHelper, privateKey common.PrivateKey, id str
 	initChainHandler()
 
 	initGameExecutor(blockChainImpl)
+	m := &transactionRequestMessage{BlockHeight: 10000, BlockPv: big.NewInt(0)}
+	go requestTransaction(*m, "111111")
 
 	return nil
 }
