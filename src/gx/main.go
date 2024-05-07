87a6cff58173cc355a25c43ee91efefd2ba14066
tequila <114778836@qq.com>
2024-05-07 10:50:56 +0800

    update log

--- src/gx/main.go
+++ src/gx/main.go
@@ -20,63 +20,51 @@ import (
 	"com.tuntun.rangers/node/src/gx/cli"
 	"fmt"
 	_ "go.uber.org/automaxprocs"
-	"os"
-	"os/signal"
 	"runtime"
 	"runtime/debug"
-	"syscall"
-	"time"
 )
+
+const gc = 30
 
 func main() {
 	initSysParam()
-	setupStackTrap()
+	//setupStackTrap()
 	gx := cli.NewGX()
 	gx.Run()
-
 }
 
 func initSysParam() {
-	debug.SetGCPercent(30)
-	debug.SetMaxStack(1 * 1000 * 1000 * 1000)
+	//maxprocs.Set(maxprocs.Logger(common.DefaultLogger.Infof))
+	debug.SetGCPercent(gc)
 
-	fmt.Printf("Setting gc %s, max memory %s, maxproc %s\n", "50", "1g", runtime.GOMAXPROCS(-1))
+	fmt.Printf("Setting gc %d, maxproc %d\n", gc, runtime.GOMAXPROCS(-1))
 }
 
-const (
-	timeFormat = "2006-01-02 15:04:05"
-)
+// const stdFile = "./stack.log"
 
-var (
-	stdFile = "./stack.log"
-)
-
-func setupStackTrap(args ...string) {
-	if len(args) > 0 {
-		stdFile = args[0]
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGUSR1)
-	go func() {
-		for range c {
-			dumpStacks()
-		}
-	}()
-}
-
-func dumpStacks() {
-	buf := make([]byte, 1638400)
-	buf = buf[:runtime.Stack(buf, true)]
-	writeStack(buf)
-}
-
-func writeStack(buf []byte) {
-	fd, _ := os.OpenFile(stdFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
-
-	now := time.Now().Format(timeFormat)
-	fd.WriteString("\n\n\n\n\n")
-	fd.WriteString(now + " stdout:" + "\n\n")
-	fd.Write(buf)
-	fd.Close()
-}
+//func setupStackTrap(args ...string) {
+//
+//	c := make(chan os.Signal, 1)
+//	signal.Notify(c, syscall.SIGUSR1)
+//	go func() {
+//		for range c {
+//			dumpStacks()
+//		}
+//	}()
+//}
+//
+//func dumpStacks() {
+//	buf := make([]byte, 1638400)
+//	buf = buf[:runtime.Stack(buf, true)]
+//	writeStack(buf)
+//}
+//
+//func writeStack(buf []byte) {
+//	fd, _ := os.OpenFile(stdFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+//
+//	now := time.Now().Format(timeFormat)
+//	fd.WriteString("\n\n\n\n\n")
+//	fd.WriteString(now + " stdout:" + "\n\n")
+//	fd.Write(buf)
+//	fd.Close()
+//}
