6d3f78508e6618d922b792a561209704014f00fc
tequila <114778836@qq.com>
2024-06-21 11:13:49 +0800

    Proposal025Block, update mainnet height

--- src/common/version.go
+++ src/common/version.go
@@ -67,7 +67,7 @@ var (
 		Proposal022Block: 62606000,
 		Proposal023Block: 63100000,
 		Proposal024Block: 62575384,
-		Proposal025Block: math.MaxUint64,
+		Proposal025Block: 63355000,
 		mainNodeContract: HexToAddress("0x74448149F549CD819b7173b6D67DbBEAFd2909a7"),
 		MysqlDSN:         "rpservice:!890rpService@#$@tcp(172.16.0.60:6666)/service?charset=utf8&parseTime=true&loc=Asia%2FShanghai",
 		JsonRPCUrl:       "https://mainnet.rangersprotocol.com/api/jsonrpc",
