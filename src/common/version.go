7dcd0020b99727695b20291b3887a3e7f1a81b05
LittleBear <1581487855@qq.com>
2024-09-04 15:29:39 +0800

    robin proposal027 set height 84150000

--- src/common/version.go
+++ src/common/version.go
@@ -106,7 +106,7 @@ var (
 		Proposal024Block: 0,
 		Proposal025Block: 77920000,
 		Proposal026Block: 79365500,
-		Proposal027Block: math.MaxUint64, //todo
+		Proposal027Block: 84150000,
 		mainNodeContract: HexToAddress("0x3a8467bEcb0B702c5c6343c8A3Ccb11acE0e8816"),
 
 		MysqlDSN:   "rpservice_v2:oJ2*bA0:hB3%@tcp(192.168.0.172:5555)/rpservice_v2?charset=utf8&parseTime=true&loc=Asia%2FShanghai",
